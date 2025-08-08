d02b3e17a2058498d6e616bb015e0d63ea80d343
James Perrin <1738333+jamesperrin@users.noreply.github.com>
2025-08-08 10:13:08 -0700

    Complete Section 2.12 Client Component & Navigation Hooks

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,10 +4,9 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold">Welcome</h1>
-      <Link href="/properties">Go To Properties</Link>
+      <Link href="/properties/1">Go To Properties</Link>
     </div>
   );
 };
 
 export default HomePage;
-
--- src/app/properties/[id]/page.jsx
+++ src/app/properties/[id]/page.jsx
@@ -1,6 +1,9 @@
-const PropertyPage = () => {
-  return <div>Propety Page</div>;
+const PropertyPage = async ({ params }) => {
+  // Dynamic APIs are Asynchronous
+  // See: https://nextjs.org/docs/messages/sync-dynamic-apis
+  const { id } = await params;
+
+  return <div>Propety Page -- id {id}</div>;
 };
 
 export default PropertyPage;
-
