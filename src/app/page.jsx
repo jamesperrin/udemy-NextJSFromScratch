aefbd847e96855efdebd04e81eb1306f3d8cf496
James Perrin <1738333+jamesperrin@users.noreply.github.com>
2025-08-11 09:54:34 -0700

    Complete Section 2.18 Home Property Listings

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 import Hero from '@/componets/Hero';
 import InfoBoxes from '@/componets/InfoBoxes';
+import HomeProperties from '@/componets/HomeProperties';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
