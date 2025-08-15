e385687741e41589ec32e0cfa4555c84adbfffa5
James Perrin <1738333+jamesperrin@users.noreply.github.com>
2025-08-15 08:04:29 -0700

    Completed Section 3.25 Single Property & Hearer Image

--- src/app/properties/[id]/page.jsx
+++ src/app/properties/[id]/page.jsx
@@ -1,9 +1,38 @@
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import PropertyHeaderImage from '@/componets/PropertyHeaderImage';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
+
 const PropertyPage = async ({ params }) => {
   // Dynamic APIs are Asynchronous
   // See: https://nextjs.org/docs/messages/sync-dynamic-apis
   const { id } = await params;
 
-  return <div>Propety Page -- id {id}</div>;
+  await connectDB();
+  const property = await Property.findById(id).lean();
+
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+            title="Go back to properties listing">
+            <FaArrowLeft className="mr-2" />
+            &nbsp;Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">{`Property Info`}</div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PropertyPage;
