5fed8f82e92c498687d6098769876dea4882bb87
James Perrin <1738333+jamesperrin@users.noreply.github.com>
2025-08-19 14:14:26 -0700

    Complete Section 5.38 Format Property Data

--- src/componets/PropertyAddForm.jsx
+++ src/componets/PropertyAddForm.jsx
@@ -3,7 +3,6 @@
 import { useRequiredFields } from '@/hooks/useRequiredFields';
 
 const PropertyAddForm = () => {
-
   useRequiredFields();
 
   return (
@@ -230,7 +229,7 @@ const PropertyAddForm = () => {
         <input
           type="text"
           id="seller_name"
-          name="seller_info.name."
+          name="seller_info.name"
           className="border rounded w-full py-2 px-3"
           placeholder="Name"
         />
