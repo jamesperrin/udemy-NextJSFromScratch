7a0d1343bc2d16629c1e3c6c09f5c0cff7d4fd5f
James Perrin <1738333+jamesperrin@users.noreply.github.com>
2025-08-21 14:14:36 -0700

    Complete Section 7.59 Saved Property Page

---
+++ src/app/properties/saved/page.jsx
@@ -0,0 +1,43 @@
+import PropertyCard from '@/components/PropertyCard';
+import connectDB from '@/config/database';
+import User from '@/models/User';
+import { getSessionUser } from '@/utils/getSessionUser';
+import { FaExclamationCircle } from 'react-icons/fa';
+
+const SavedPropertiesPage = async () => {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    return { error: 'User ID is required' };
+  }
+
+  const { userId } = sessionUser;
+
+  // NOTE: here we can make one database query by using Model.populate
+  const { bookmarks } = await User.findById(userId).populate('bookmarks').lean();
+
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <h1 className="text-2xl mb-4 font-bold">Saved Properties</h1>
+        {bookmarks.length === 0 ? (
+          <p className="font-bold">
+            <i>
+              <FaExclamationCircle className="text-yellow-400 text-2xl fa-5x inline-block mr-2" />
+            </i>
+            &nbsp;No saved properties
+          </p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {bookmarks.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+export default SavedPropertiesPage;
